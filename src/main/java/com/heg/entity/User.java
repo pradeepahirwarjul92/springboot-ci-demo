9c3af1ad2509153eb9cdd3c8d4ae2d84b1c68ccd
anchit <anchitchourasia78@gmail.com>
2026-04-18 15:17:31 +0530

    4

--- src/main/java/com/heg/entity/User.java
+++ src/main/java/com/heg/entity/User.java
@@ -1,42 +1,47 @@
 package com.heg.entity;
 
+import java.io.Serializable; // <-- 1. Add this import
 
+// 2. Add "implements Serializable" to the class
+public class User implements Serializable {
+    
+    // 3. Recommended: Add a serialVersionUID for version control of the object
+    private static final long serialVersionUID = 1L;
 
+    private Long id;
+    private String name;
+    private String email;
 
-public class User {
-	
-	
-	    private Long id;
+    // Existing Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	    private String name;
-	    private String email;
-		public Long getId() {
-			return id;
-		}
-		public void setId(Long id) {
-			this.id = id;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
-		public User(Long id, String name, String email) {
-			super();
-			this.id = id;
-			this.name = name;
-			this.email = email;
-		}
-		
-		
-		
-	
+    // Default constructor (required for serialization)
+    public User() {
+        super();
+    }
 
+    // Your existing constructor
+    public User(Long id, String name, String email) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
