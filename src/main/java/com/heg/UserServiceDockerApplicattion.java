007badff4a9d9813e7220f5c95d299907dd46822
anchit <anchitchourasia78@gmail.com>
2026-04-18 14:59:13 +0530

    #

--- src/main/java/com/heg/UserServiceDockerApplicattion.java
+++ src/main/java/com/heg/UserServiceDockerApplicattion.java
@@ -4,18 +4,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching; // <-- 1. Add this import
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @SpringBootApplication
-@EnableCaching // <-- 2. Add this annotation to activate Redis caching
+@EnableCaching 
 public class UserServiceDockerApplicattion extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(UserServiceDockerApplicattion.class);
-	}
+    // These fields will capture the environment variables from your Ansible playbook
+    @Value("${SPRING_REDIS_HOST:localhost}")
+    private String redisHost;
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceDockerApplicattion.class, args);
-	}
+    @Value("${SPRING_REDIS_PORT:6379}")
+    private int redisPort;
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserServiceDockerApplicattion.class);
+    }
+
+    // This Bean forces the application to connect to the "redis-cache" container
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceDockerApplicattion.class, args);
+    }
 }
